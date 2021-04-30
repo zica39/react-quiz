6b9303a1be32633c8f2e241280c767fb2177a8f3
Zeljko Ivanovic <zeljkoivanovic369@gmail.com>
2021-04-30 16:01:16 +0200

    Created empty react project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import Quiz from "./pages/quiz/Quiz";
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <Quiz/>
+
+    </div>
+  );
+}
+
+export default App;
