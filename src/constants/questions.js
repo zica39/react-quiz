01d8b9273827e31a41ac39d371bc6ab8470f5828
Zeljko Ivanovic <zeljkoivanovic369@gmail.com>
2021-04-30 16:08:11 +0200

    Create questions.js

---
+++ src/constants/questions.js
@@ -0,0 +1,189 @@
+const questions = [
+    {
+        id: 1,
+        question: 'Koliko ima mostova na reci Amazon',
+        1:'17',
+        2:'23',
+        3:'64',
+        4:'nema ni jedan',
+        correct:4
+    },
+    {
+        id: 2,
+        question: 'Sta znaci skraćenica DVR',
+        1:'Digital Video Reading',
+        2:'Digital Verification Recorder',
+        3:'Digital Video Recorder',
+        4:'Digital Video Rooting',
+        correct:3
+    },
+    {
+        id: 3,
+        question: 'Grčka čegertaljka ili:',
+        1:'Vibrafon',
+        2:'Klepetuša',
+        3:'Krotalon',
+        4:'Čegrtuša',
+        correct:3
+    },
+    {
+        id: 4,
+        question: 'Samjuel Lenghorn Klemens je pravo ime književnika:',
+        1:'Stendala',
+        2:'Đžek London',
+        3:'Vilijam Foknera',
+        4:'Mark Tvena',
+        correct:4
+    },
+    {
+        id: 5,
+        question: '"Mali Pas" je:',
+        1:'Sazvežđe',
+        2:'Kanarsko ostrvo',
+        3:'Reka u Americi',
+        4:'Planina',
+        correct:1
+    },
+    {
+        id: 6,
+        question: 'Ko je bio pobjednik Evrovizije 2003.godine?',
+        1:'Belgija',
+        2:'Švedska',
+        3:'Turska',
+        4:'Rusija',
+        correct:3
+    },
+    {
+        id: 7,
+        question: 'Zvanična valuta u BiH je:',
+        1:'Euro',
+        2:'Marka',
+        3:'Denvar',
+        4:'Dinar',
+        correct:2
+    },
+    {
+        id: 8,
+        question: 'U imenu Coca-cola, reč Coca označava:',
+        1:'Karoten',
+        2:'Ksiliotol',
+        3:'Kofein',
+        4:'Kokain',
+        correct:4
+    },
+    {
+        id: 9,
+        question: 'Koja od ovih grupa nije iz Hrvatske?',
+        1:'Zana',
+        2:'Srebrna Krila',
+        3:'Novi Fosili',
+        4:'Magazin',
+        correct:1
+    },
+    {
+        id: 10,
+        question: 'Sinus ugla od 90 stepeni iznosi:',
+        1:'0',
+        2:'1',
+        3:'0.5',
+        4:'3.14',
+        correct:2
+    },
+    {
+        id: 11,
+        question: 'Roman "Gospođica" je napisao:',
+        1:'Dobrica Ćosić',
+        2:'Meša Selimović',
+        3:'Ivo Andrić',
+        4:'Slobodan Selenić',
+        correct:3
+    },
+
+    {
+        id: 12,
+        question: 'Ko je režirao film o Bob Dilanu, "No Direction Home"?',
+        1:'Martin Skorseze',
+        2:'Ron Howard',
+        3:'Bob Dilan',
+        4:'Stiven Spilberg',
+        correct:1
+    },
+
+    {
+        id: 13,
+        question: 'Kroz koji grad protiče rijeka Neva',
+        1:'Bukurešt',
+        2:'Berlin',
+        3:'Sankt Petersburg',
+        4:'Ženeva',
+        correct:3
+    },
+    {
+        id: 14,
+        question: 'Kule Petronas se nalaze:',
+        1:'Kuala Lumpuru',
+        2:'Njujorku',
+        3:'Hong Kongu',
+        4:'Otavi',
+        correct:1
+    },
+
+    {
+        id: 15,
+        question: 'Kako se naziva mjesto u zemljinoj kori gdije nastaju zemljotresi?',
+        1:'Zemicentar',
+        2:'Seizmocentar',
+        3:'Hipocentar',
+        4:'Epicentar',
+        correct:3
+    },
+    {
+        id: 16,
+        question: 'Američki predsednik Nikson bio je prinuđen da se povuče sa tog položaja zbog učešća u aferi:?',
+        1:'Votergejt',
+        2:'Staviski',
+        3:'Simpson',
+        4:'Profjumo',
+        correct:1
+    },
+    {
+        id: 17,
+        question: 'Tasmaniski vuk je:',
+        1:'Torbar',
+        2:'Pas',
+        3:'Glodar',
+        4:'Lisica',
+        correct:1
+    },
+    {
+        id: 18,
+        question: 'Riječ "inertan" u prevodu sa latinskog znači:',
+        1:'Jako brz',
+        2:'Brz',
+        3:'Trom',
+        4:'Nestabilan',
+        correct:3
+    },
+    {
+        id: 19,
+        question: 'Hemijska oznaka za živu je:',
+        1:'Hg',
+        2:'Pb',
+        3:'Lv',
+        4:'Zv',
+        correct:1
+    },
+    {
+        id: 20,
+        question: 'Ko je osnovao grad Keln?',
+        1:'Rimljani',
+        2:'Germani',
+        3:'Gali',
+        4:'Goti',
+        correct:1
+    }
+
+
+]
+
+export default questions;
